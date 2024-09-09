d4a7db6a8705113f55126ae5ccfef74e39691d9a
Adam Dangoor <adamdangoor@gmail.com>
2024-09-09 23:05:10 +0100

    Add stubs for tests

---
+++ tests/test_docrun.py
@@ -0,0 +1,13 @@
+"""Tests for `docrun`."""
+
+
+def test_help() -> None:
+    """Test the help message."""
+
+
+def test_run_command() -> None:
+    """Test running a command."""
+
+
+def test_error() -> None:
+    """Test an error."""
