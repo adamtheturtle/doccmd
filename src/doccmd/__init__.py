7163278890bc932e58a8cf34a64739278d7e6c18
Adam Dangoor <adamdangoor@gmail.com>
2025-03-25 08:46:45 +0000

    Progress towards simpler main

--- src/doccmd/__init__.py
+++ src/doccmd/__init__.py
@@ -469,6 +469,30 @@ class _GroupModifiedError(Exception):
         self.example = example
         self.modified_example_content = modified_example_content
 
+    def __str__(self) -> str:
+        """
+        Get the string representation of the error.
+        """
+        unified_diff = difflib.unified_diff(
+            a=str(object=self.example.parsed).lstrip().splitlines(),
+            b=self.modified_example_content.lstrip().splitlines(),
+            fromfile="original",
+            tofile="modified",
+        )
+        message = textwrap.dedent(
+            text=f"""\
+            Writing to a group is not supported.
+
+            A command modified the contents of examples in the group ending on line {self.example.line} in {Path(self.example.path).as_posix()}.
+
+            Diff:
+
+            """,  # noqa: E501
+        )
+
+        message += "\n".join(unified_diff)
+        return message
+
 
 @beartype
 def _raise_group_modified(
@@ -988,28 +1012,10 @@ def main(
                 if fail_on_parse_error:
                     sys.exit(1)
             except _GroupModifiedError as exc:
-                unified_diff = difflib.unified_diff(
-                    a=str(object=exc.example.parsed).lstrip().splitlines(),
-                    b=exc.modified_example_content.lstrip().splitlines(),
-                    fromfile="original",
-                    tofile="modified",
-                )
-                message = textwrap.dedent(
-                    text=f"""\
-                    Writing to a group is not supported.
-
-                    A command modified the contents of examples in the group ending on line {exc.example.line} in {Path(exc.example.path).as_posix()}.
-
-                    Diff:
-
-                    """,  # noqa: E501
-                )
-
-                message += "\n".join(unified_diff)
                 if fail_on_group_write:
-                    _log_error(message=message)
+                    _log_error(message=str(object=exc))
                     sys.exit(1)
-                _log_warning(message=message)
+                _log_warning(message=str(object=exc))
             except _EvaluateError as exc:
                 if exc.reason:
                     message = (
