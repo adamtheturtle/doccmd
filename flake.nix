99741fe11cab9d2b9b10020f7a8be8b1f2cee1f5
Adam Dangoor <adamdangoor@gmail.com>
2026-01-20 21:28:38 +0000

    Use uv2nix to read dependencies from uv.lock
    This eliminates manual dependency version maintenance in flake.nix.
    The uv.lock file is now tracked in git as required by uv2nix.

--- flake.nix
+++ flake.nix
@@ -3,153 +3,115 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        python = pkgs.python312;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
 
-        # Version for setuptools-scm: use git short rev for dev builds
-        version = "0.0.0+${self.shortRev or self.dirtyShortRev or "unknown"}";
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
 
-        # Override setuptools-scm to a newer version required by click-compose
-        setuptools-scm-new = python.pkgs.setuptools-scm.overridePythonAttrs (old: rec {
-          version = "9.2.2";
-          src = python.pkgs.fetchPypi {
-            pname = "setuptools_scm";
-            inherit version;
-            hash = "sha256-HGdKtGZWhqCIfX4kwDqyXyQgHCE+guponS8+Fp7371c=";
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+
+      # Override doccmd to set the version from git
+      doccmdOverlay = final: prev: {
+        doccmd = prev.doccmd.overrideAttrs (old: {
+          env = (old.env or { }) // {
+            SETUPTOOLS_SCM_PRETEND_VERSION = "0.0.0+${self.shortRev or self.dirtyShortRev or "unknown"}";
           };
         });
+      };
 
-        # Override beartype to a newer version required by sybil-extras
-        beartype-new = python.pkgs.beartype.overridePythonAttrs (old: rec {
-          version = "0.22.9";
-          src = python.pkgs.fetchPypi {
-            pname = "beartype";
-            inherit version;
-            hash = "sha256-j4K1SqcjooSKVgCNGIdfkcHbAsMu9qYjGaAC4+Jal18=";
+      pythonSets = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          python = pkgs.python312;
+        in
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope
+          (
+            lib.composeManyExtensions [
+              pyproject-build-systems.overlays.wheel
+              overlay
+              doccmdOverlay
+            ]
+          )
+      );
+
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pythonSet = pythonSets.${system};
+          virtualenv = pythonSet.mkVirtualEnv "doccmd-env" workspace.deps.default;
+        in
+        {
+          default = virtualenv;
+          doccmd = virtualenv;
+        }
+      );
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/doccmd";
+        };
+      });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          pythonSet = pythonSets.${system};
+          virtualenv = pythonSet.mkVirtualEnv "doccmd-dev-env" workspace.deps.all;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              virtualenv
+              pkgs.uv
+            ];
+            env = {
+              UV_NO_SYNC = "1";
+              UV_PYTHON = pythonSet.python.interpreter;
+              UV_PYTHON_DOWNLOADS = "never";
+            };
+            shellHook = ''
+              unset PYTHONPATH
+            '';
           };
-        });
-
-        # Override sybil to a newer version required by sybil-extras
-        sybil-new = python.pkgs.sybil.overridePythonAttrs (old: rec {
-          version = "9.3.0";
-          src = python.pkgs.fetchPypi {
-            pname = "sybil";
-            inherit version;
-            hash = "sha256-hH0dF7ioV8S7P4RxtKV7iv+pOaYPv1B+cKpyrXkJfAU=";
-          };
-        });
-
-        click-compose = python.pkgs.buildPythonPackage rec {
-          pname = "click-compose";
-          version = "2025.10.27.3";
-          pyproject = true;
-
-          src = python.pkgs.fetchPypi {
-            pname = "click_compose";
-            inherit version;
-            hash = "sha256-bTMmoTtpCseg8Omd54WqeOqB0TC6AtYJ5jZ6evI0d6U=";
-          };
-
-          build-system = [
-            python.pkgs.setuptools
-            setuptools-scm-new
-          ];
-
-          dependencies = [
-            beartype-new
-            python.pkgs.click
-          ];
-
-          pythonImportsCheck = [ "click_compose" ];
-        };
-
-        sybil-extras = python.pkgs.buildPythonPackage rec {
-          pname = "sybil-extras";
-          version = "2025.12.13.4";
-          pyproject = true;
-
-          src = python.pkgs.fetchPypi {
-            pname = "sybil_extras";
-            inherit version;
-            hash = "sha256-OMrM4XJEE/O3AfhAAQppUIjWfoYC6tZlMOMW4Q7ytlE=";
-          };
-
-          build-system = [
-            python.pkgs.setuptools
-            setuptools-scm-new
-          ];
-
-          dependencies = [
-            beartype-new
-            sybil-new
-          ];
-
-          pythonImportsCheck = [ "sybil_extras" ];
-        };
-
-        doccmd = python.pkgs.buildPythonApplication {
-          pname = "doccmd";
-          inherit version;
-          pyproject = true;
-
-          src = ./.;
-
-          build-system = with python.pkgs; [
-            setuptools
-            setuptools-scm
-          ];
-
-          dependencies = with python.pkgs; [
-            charset-normalizer
-            click
-            cloup
-            pygments
-          ] ++ [
-            beartype-new
-            click-compose
-            sybil-extras
-            sybil-new
-          ];
-
-          # Skip tests as they require many additional dependencies
-          doCheck = false;
-
-          # setuptools-scm needs git - use git rev for dev builds
-          SETUPTOOLS_SCM_PRETEND_VERSION = version;
-
-          pythonImportsCheck = [ "doccmd" ];
-
-          meta = with pkgs.lib; {
-            description = "Run commands against code blocks in documentation files";
-            homepage = "https://github.com/adamtheturtle/doccmd";
-            license = licenses.mit;
-            maintainers = [ ];
-            mainProgram = "doccmd";
-          };
-        };
-      in
-      {
-        packages = {
-          default = doccmd;
-          doccmd = doccmd;
-        };
-
-        apps.default = flake-utils.lib.mkApp {
-          drv = doccmd;
-        };
-
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ doccmd ];
-          packages = [
-            python
-          ];
-        };
-      }
-    );
+        }
+      );
+    };
 }
